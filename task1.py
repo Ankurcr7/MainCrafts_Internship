6ec9aa039df1bdc29155b5b101875d096acfa5e8
Ankurcr7 <ankurdas140905@gmail.com>
2025-09-16 22:23:06 +0530

    update

--- task1.py
+++ task1.py
@@ -1,4 +1,4 @@
-# Sum of two numbers
+# 1. Sum of two numbers
 """
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
@@ -6,7 +6,7 @@ b = int(input("Enter second number: "))
 print(f"{a} + {b} = {a+b}")
 """
 
-# Odd or Even checker
+# 2. Odd or Even checker
 """
 num = int(input("Enter a number: "))
 if num % 2 == 0:
@@ -16,4 +16,34 @@ else:
     
 """
 
-# 
+# 3. Factorial Calculation
+
+"""
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
+
+num = int(input("Enter a number: "))
+print(f"{num}! = {factorial(num)}")
+
+"""
+
+
+# 4. Fibonacci Sequence
+"""
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n ==1 :
+        return 1
+    
+    return fibonacci(n-1) + fibonacci(n-2)
+    
+
+terms = int(input("Upto which term: "))
+for i in range(terms):
+    print(fibonacci(i) , end=" ")
+
+"""
+
