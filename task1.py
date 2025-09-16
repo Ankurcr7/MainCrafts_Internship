13d73624bca06f24b640382f86c3fa3baf0f21fb
Ankurcr7 <ankurdas140905@gmail.com>
2025-09-16 14:18:49 +0530

    update

---
+++ task1.py
@@ -0,0 +1,19 @@
+# Sum of two numbers
+"""
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(f"{a} + {b} = {a+b}")
+"""
+
+# Odd or Even checker
+"""
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} is an Even number")
+else:
+    print(f"{num} is an Odd number")
+    
+"""
+
+# 
