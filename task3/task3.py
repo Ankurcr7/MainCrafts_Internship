66dc4c3edc9f933c4f17c77da2ec4cda3bf8e2fe
Ankurcr7 <ankurdas140905@gmail.com>
2025-10-10 22:08:30 +0530

    completed

--- task3/task3.py
+++ task3/task3.py
@@ -28,18 +28,40 @@ def view_expenses() -> None:
 
 
 def search_category(category:str) -> None:
-    pass
+    try:
+        with open("expenses.csv", "r") as f:
+            for i in csv.reader(f):
+                if i[2].lower() == category.lower():
+                    print(i)
+
+            print()
+
+    except FileNotFoundError:
+        print("File not found or No expenses found!")
 
 
+def total_spent_by_category(category:str) -> None:
+    try:
+        with open("expenses.csv", "r") as f:
+            sum = 0
+            for i in csv.reader(f):
+                if i[2].lower() == category.lower():
+                    sum += float(i[1])
+            print(f"Total spent in {category}: {sum:.2f}\n")
+    except FileNotFoundError:
+        print("No expenses found!")
+                
 
-def total_spent_by_category():
-    pass
-
-
-
-def total_monthly_spent():
-    pass
-
+def total_monthly_spent(month: str) -> None:
+    try:
+        with open("expenses.csv", "r") as f:
+            sum = 0
+            for i in csv.reader(f):
+                if i[3].startswith(month):
+                    sum += float(i[1])
+            print(f"Monthly total: {sum:.2f}\n")
+    except FileNotFoundError:
+        print("No expenses found!")
 
 
 def total_expenses() -> None:
@@ -49,10 +71,9 @@ def total_expenses() -> None:
             for i in csv.reader(f):
                 sum += float(i[1])
 
-        print(f"Total Expenses: Rs {sum:.2f}\n")
+        print(f"Total Expenses: {sum:.2f}\n")
     except FileNotFoundError:
         print("No expenses found!")
-
 
 
 
@@ -60,7 +81,7 @@ print("Welcome to Expense Tracker\n")
 
 while True:
     print("Select an option from below:-")
-    print("[1] Add Expense \n[2] View Expenses \n[3] Search Category \n[4] Total spent per Category \n[5] Total monthly spent")
+    print("[1] Add Expense \n[2] View Expenses \n[3] Search Category \n[4] Total spent per Category \n[5] Total monthly spent \n[6] Total Expenses \n")
     
     user = input("Type the option's number or type any character to EXIT: ")
     if user == "1":
@@ -77,10 +98,15 @@ while True:
         search_category(category)
 
     elif user == "4":
-        ...
+        category = input("Enter a Category: ")
+        total_spent_by_category(category)
 
     elif user == "5":
-        ...
+        month = input("Enter month(YYYY-MM): ")
+        total_monthly_spent(month)
+
+    elif user == "6":
+        total_expenses()
 
     else:
         break
